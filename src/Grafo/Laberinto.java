28a87808f2abf2780b92780b350f74cded67b4f9
LENOVO1 <LENOVO1@Lenovo-PC>
2018-12-02 17:53:41 -0500

    Initial commit

--- src/Grafo/Laberinto.java
+++ src/Grafo/Laberinto.java
@@ -5,6 +5,8 @@
  */
 package Grafo;
 
+import LinearDataStructures.ExampleNode;
+import LinearDataStructures.List;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -20,10 +22,31 @@ public class Laberinto {
  public static BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
  public static BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
  
+ public static List n= new List();
  public static int Fila=9,Columna=9;
  public static int [][] matrix = new int[Fila][Columna];
- public static int bandera=0;
+ public static int bandera=0,tem=0, tem1=0, tem2=0, tem3=0, posibleFila=0, posibleColumna=0;
+         
  
+ public static void imprimir() throws IOException
+ {
+  for (int i = 0; i < Fila; i++) 
+   {
+      for (int j = 0; j < Columna; j++) {
+         bw.write(matrix[i][j]+" ");
+               
+      }
+      bw.write("\n");
+   }
+   bw.flush();
+   
+   bw.write("\n");
+   bw.flush();
+ 
+   
+   
+ }
+         
  public static void crear_laberinto() throws IOException
  {
    // 2 es la salida
@@ -32,6 +55,7 @@ public class Laberinto {
    matrix[1][3]=3;
   
   //estos son los muros
+
    matrix[0][4]= 1;
    matrix[0][6]= 1;
    matrix[1][7]= 1;
@@ -75,163 +99,186 @@ public class Laberinto {
    matrix[8][7]= 1;
    matrix[8][8]= 1;
    
-
-        
-   for (int i = 0; i < Fila; i++) 
-   {
-      for (int j = 0; j < Columna; j++) {
-         bw.write(matrix[i][j]+" ");
-               
-      }
-      bw.write("\n");
-   }
-   bw.flush();
+   imprimir();
  }
  
  public static void recorrido() throws IOException
  {
-  //do{
-  int FilaActual=0,ColumnaActual=0;
+  int Fila=0,Columna=0 ;
    for (int i =0;i<Fila;i++)
    {
      for (int j=0;j<Columna;j++)
      {
       if (matrix[i][j]==2)
       {
-       FilaActual=i;
-       ColumnaActual=j;
+       Fila=i;
+       Columna=j;
       }
      }
    }
-   int posibleFila=FilaActual, posibleColumna=ColumnaActual;
-   do
-   {
-    int tem=posibleColumna-1;
-    int tem1=posibleColumna+1;
-    int tem2=posibleFila-1;
-    int tem3=posibleFila+1;
-    bandera=0;
-    
-    // posicion izquierda
-    if( tem >=0)
-    {
-     if (matrix[posibleFila][tem]!=1)
-     {
-      bandera=1;
-      if((matrix[posibleFila][posibleColumna-1]==0)||(matrix[posibleFila][posibleColumna-1]==3))
-      {
-       posibleColumna=posibleColumna-1;
-       if((matrix[posibleFila][posibleColumna]==0))
-       {
-        matrix[posibleFila][posibleColumna]=4;
-       }
-      }
-     }
-     else
-     {
-      posibleColumna=tem;
-     }
-    }
-    
-    //posicion derecha
-    //if(bandera==0)
-    //{
-     if(tem1<=8)
-     {
-      if(matrix[posibleFila][tem1]!=1)
-      {
-       bandera=1;
-       if((matrix[posibleFila][posibleColumna+1]==0)||(matrix[posibleFila][posibleColumna+1]==3))
-       {
-        posibleColumna=posibleColumna+1;
-        if((matrix[posibleFila][posibleColumna]==0))
-        {
-         matrix[posibleFila][posibleColumna]=4;
+  
+        String direccion = "derecha";
+        Fila=6;
+        Columna=2;
+        //n.insertAtEnd(new ExampleNode(matrix[1][3])); 
+        while (matrix[Fila][Columna] !=3 && bandera!=1) {
+            imprimir();
+            switch (direccion) {
+                case "derecha"://DERECHA
+                    if (Fila - 1>=0 && matrix[Fila - 1][Columna]!=1 && (matrix[Fila - 1][Columna]==0||matrix[Fila - 1][Columna]==4)) 
+                    {
+                        matrix[Fila - 1][Columna] = 4;
+                        //matrix[Fila][Columna] = 0;
+                        Fila--;
+                        direccion = "arriba";
+                    } 
+                    else 
+                    {
+                     if(Fila - 1>=0 &&matrix[Fila - 1][Columna] ==3)
+                     {
+                      Fila = Fila - 1;
+                      bandera=1;
+                      break;
+                     }
+                        if (Columna + 1<=8 && matrix[Fila][Columna + 1]!=1&& (matrix[Fila][Columna + 1]==0||matrix[Fila][Columna + 1]==4)) 
+                        {
+                            matrix[Fila][Columna + 1] = 4;
+                            //matrix[Fila][Columna] = 0;
+                            Columna++;
+                        } else {
+                            if (Fila + 1<=8 && matrix[Fila + 1][Columna]!=1&& (matrix[Fila + 1][Columna]==0||matrix[Fila + 1][Columna]==4)) {
+                                matrix[Fila + 1][Columna] = 4;
+                               // matrix[Fila][Columna] = 0;
+                                Fila++;
+                                direccion = "abajo";
+                            } else {
+                                matrix[Fila][Columna - 1] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Columna--;
+                                direccion = "izquierda";
+                            }
+                        }
+                    }
+                    break;
+                case "arriba"://ARRIBA
+                    if (Columna - 1>=0 && matrix[Fila][Columna - 1]!=1&& (matrix[Fila][Columna - 1]==0||matrix[Fila][Columna - 1]==4)) 
+                    {
+                        matrix[Fila][Columna - 1] = 4;
+                        //matrix[Fila][Columna] = 0;
+                        Columna--;
+                        direccion = "izquierda";
+                    } 
+                    else 
+                    {
+                     if (Columna - 1>=0 &&matrix[Fila][Columna - 1] ==3)
+                     {
+                      Columna =Columna - 1;
+                      bandera=1;
+                      break;
+                     }
+                        if (Fila - 1>=0 && matrix[Fila - 1][Columna]!=1 && (matrix[Fila - 1][Columna]==0||matrix[Fila - 1][Columna]==4)) 
+                        {
+                            matrix[Fila - 1][Columna] = 4;
+                           // matrix[Fila][Columna] = 0;
+                            Fila--;
+                        } else {
+                            if (Columna + 1<=8 && matrix[Fila][Columna + 1]!=1 && (matrix[Fila][Columna + 1]==0||matrix[Fila][Columna + 1]==4)) 
+                            {
+                                matrix[Fila][Columna + 1] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Columna++;
+                                direccion = "derecha";
+                            } else {
+                                matrix[Fila + 1][Columna] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Fila++;
+                                direccion = "abajo";
+                            }
+                        }
+                    }
+                    break;
+                case "izquierda":
+                    if (Fila + 1<=8 && matrix[Fila + 1][Columna]!=1 && (matrix[Fila + 1][Columna]==0||matrix[Fila + 1][Columna]==4)) {
+                        matrix[Fila + 1][Columna] = 4;
+                       // matrix[Fila][Columna] = 0;
+                        Fila++;
+                        direccion = "abajo";
+                    } 
+                    else 
+                    {
+                     if (Fila + 1<=8 &&matrix[Fila + 1][Columna] ==3)
+                     {
+                      bandera=1;
+                       Fila =Fila + 1;
+                     }
+                        if (Columna - 1>=0 && matrix[Fila][Columna - 1]!=1 &&(matrix[Fila][Columna - 1]==0||matrix[Fila][Columna - 1]==4)) {
+                            matrix[Fila][Columna - 1] = 4;
+                            //matrix[Fila][Columna] = 0;
+                            Columna--;
+                        } else {
+                            if (Fila - 1>=0 && matrix[Fila - 1][Columna]!=1&& (matrix[Fila - 1][Columna]==0||matrix[Fila - 1][Columna]==4)) {
+                                matrix[Fila - 1][Columna] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Fila--;
+                                direccion = "arriba";
+                            } else {
+                                matrix[Fila][Columna + 1] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Columna++;
+                                direccion = "derecha";
+                            }
+                        }
+                    }
+                    break;
+
+                case "abajo"://ARRIBA
+                    if (Columna + 1<=8 && matrix[Fila][Columna + 1]!=1 && (matrix[Fila][Columna + 1]==0||matrix[Fila][Columna + 1]==4)) {
+                        matrix[Fila][Columna + 1] = 4;
+                        //matrix[Fila][Columna] = 0;
+                        Columna++;
+                        direccion = "derecha";
+                    } 
+                    else 
+                    {
+                     if(Columna + 1<=8 &&matrix[Fila][Columna + 1] ==3)
+                     {
+                      bandera=1;
+                       Columna= Columna + 1;
+                       break;
+                     }
+                        if (Fila + 1<=8 && matrix[Fila + 1][Columna]!=1 && (matrix[Fila + 1][Columna]==0||matrix[Fila + 1][Columna]==4)) {
+                            matrix[Fila + 1][Columna] = 4;
+                            //matrix[Fila][Columna] = 0;
+                            Fila++;
+                        } else {
+                            if (Columna - 1>=0 && matrix[Fila][Columna - 1]!=1 && (matrix[Fila][Columna - 1]==0||matrix[Fila][Columna - 1]==4)) {
+                                matrix[Fila][Columna - 1] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Columna--;
+                                direccion = "izquierda";
+                            } else {
+                                matrix[Fila - 1][Columna] = 4;
+                                //matrix[Fila][Columna] = 0;
+                                Fila--;
+                                direccion = "arriba";
+                            }
+                        }
+                    }
+                    break;
+                default:
+            }
         }
-       }
-      }
-      else
-      {
-       posibleColumna=tem1;
-      }
-     }
-    //}
-    
-    //posicion arriba
-    //if(bandera==0)
-    //{
-     if (tem2>=0)
-     {
-      if(matrix[tem2][posibleColumna]!=1)
-      {
-       bandera=1;
-       if((matrix[posibleFila-1][posibleColumna]==0)||(matrix[posibleFila-1][posibleColumna]==3))
-        {
-         posibleFila=posibleFila-1;
-         if((matrix[posibleFila][posibleColumna]==0))
-         {
-          matrix[posibleFila][posibleColumna]=4;
-         }
-        }
-      }
-      else
-      {
-        posibleFila=tem2;
-      }
-     }
-    //}
-    
-    // posicion abajo
-    //if(bandera==0)
-    //{
-     if(tem3<=8)
-     {
-      if(matrix[tem3][posibleColumna]!=1)
-      {
-       bandera=1;
-       if((matrix[posibleFila+1][posibleColumna]==0)||(matrix[posibleFila+1][posibleColumna]==3))
-         {
-          posibleFila=posibleFila+1;
-          if((matrix[posibleFila][posibleColumna]==0))
-          {
-           matrix[posibleFila][posibleColumna]=4;
-          }
-         }
-      }else
-      {
-       posibleFila=tem3;
-      }
-     //}
-     
-     
-    }
-    bw.write(bandera+"\n");
-   bw.flush();
-   }while(matrix[posibleFila][posibleColumna]==1);
-   
-   
-   bw.write(bandera+"\n");
-   bw.flush();
-   
-   for (int i = 0; i < Fila; i++) 
-   {
-      for (int j = 0; j < Columna; j++) {
-         bw.write(matrix[i][j]+" ");
-               
-      }
-      bw.write("\n");
-   }
-   bw.flush();
- // }while(matrix[posibleFila][posibleColumna]==3)
+  //........................................................................................
+  
  }
- 
- public static void main (String []args) throws IOException
+
+
+ public static void main (String []args) throws IOException 
  {
    crear_laberinto();
-   
-   for(int i =0; i<10;i++){
+
     recorrido();
-   }
+   
    
  }
  
